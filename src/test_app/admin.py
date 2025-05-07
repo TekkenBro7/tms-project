cf1e687450a5d33aa7685028857441f9247beea9
Maxim Sniazhko <maks.kreet@mail.ru>
2025-05-07 20:15:30 +0300

    Added TestExample model

--- src/test_app/admin.py
+++ src/test_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TestExample
 
-# Register your models here.
+
+@admin.register(TestExample)
+class TestExampleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')  
+    search_fields = ('name',)
--- src/test_app/models.py
+++ src/test_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TestExample(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Example'
+        verbose_name_plural = 'Examples'
