e038f2441bbede7b935c379e098ee1d005dca232
Maxim Sniazhko <maks.kreet@mail.ru>
2025-06-11 18:33:01 +0300

    Fix changes

--- src/core/settings.py
+++ src/core/settings.py
@@ -203,8 +203,8 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 # Email notification settings
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
